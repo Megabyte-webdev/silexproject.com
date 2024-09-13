9c48966ec58fc4ace4a621ac1c0a31ac09c5f60f
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-09-13 17:45:51 +0100

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='py-4 bg-black text-white min-h-96 w-[98%] mx-auto md:w-full'>
-        <div className='w-[90%] mx-auto border-b-2 border-gray-400 py-4 my-6 flex flex-wrap justify-between items-center'>
+        <div className='w-[90%] mx-auto border-b-2 border-gray-400 py-4 my-6 flex flex-wrap justify-between items-end'>
             <section className='h-40 rounded-md flex-grow md:w-48 max-w-96 w-[90%] p-6 m-4 transition-all ease-in-out duration-300'>
-                <h2 className='text-xl md:text-2xl text-white'>Ready to Transform your Business?<br/>Take the first step Today!</h2>
+                <h2 className='text-xl text-white'>Ready to Transform your Business?<br/>Take the first step Today!</h2>
                 <p>Totam rem aperiam, eaque ipsa quae ab lilo inventore veritatis et quasi architecto baetae vitae dicta sunt.</p>
             </section>
-            <section className='flex-grow flex flex-col justify-end h-24 rounded-md md:w-max max-w-48 w-[90%] p-6 m-4 transition-all ease-in-out duration-300'>
+            <section className='flex-grow flex flex-col justify-end h-max rounded-md md:w-max max-w-48 w-[90%] p-6 m-4 transition-all ease-in-out duration-300'>
                 <p className='text-gray-500'>Call Us</p>
                 <a className='text-xl text-blue-600' href="tel:+23481062344890">+234 810 6234 4890</a>
             </section>
-            <section className='flex-grow flex flex-col justify-end h-24 rounded-md md:w-max max-w-48 w-[90%] p-6 m-4 transition-all ease-in-out duration-300'>
+            <section className='flex-grow flex flex-col justify-end h-max rounded-md md:w-max max-w-48 w-[90%] p-6 m-4 transition-all ease-in-out duration-300'>
                 <p className='text-gray-500 text-sm'>Email Us</p>
                 <a className='text-xl text-blue-600' href="mailto:cyber@silexsecure.com">cyber@silexsecure.com</a>
             </section>
