d41eb6ab0fa6489907ed8026dbb394f6fd32f062
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-09-17 23:48:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,9 +16,8 @@ function App() {
         <Route index element={<MainPage />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-        {
-          user !== null &&  <Route path='/verify-otp' element={<OTPVerification />} />
-        }
+       <Route path='/verify-otp' element={<OTPVerification />} />
+        
         {
           user !== null && <Route path="/user" element={<UserPage />} />
         }
