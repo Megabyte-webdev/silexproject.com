909f83da763a7d2fb2fc89c008c1de75fad8f902
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-09-18 00:24:56 +0100

    Update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -31,7 +31,7 @@ const Signup = () => {
   // Handler for OTP resend
   const handleVerification = (e) => {
     e.preventDefault();
-    navigate("/verify-otp?email=${email}&fromSignUp=true");
+    navigate(`/verify-otp?email=${email}&fromSignUp=true`);
   };
 
   return (
