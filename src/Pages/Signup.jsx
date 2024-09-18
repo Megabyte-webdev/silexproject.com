3f0dfa2bcfe4a52e7edefc6ace8406596c6bb837
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-09-18 10:17:06 +0100

    Update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -26,7 +26,6 @@ const Signup = () => {
       return;
     }
     handleRegister({ email, username, password, password_confirmation: confirmPassword });
-alert(user)
   };
 
   // Handler for OTP resend
